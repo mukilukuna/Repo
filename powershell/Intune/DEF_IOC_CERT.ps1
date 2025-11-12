b79d900f4f61e73951b0f9ebf69dcc49088c335f
Muki <muki.lukuna@outlook.com>
2025-11-12 15:31:11 +0100

    Update file paths in DEF_IOC_CERT.ps1 for UniLogicSetup certificate extraction

--- powershell/Intune/DEF_IOC_CERT.ps1
+++ powershell/Intune/DEF_IOC_CERT.ps1
@@ -1,8 +1,8 @@
 # Pad van het geblokkeerde bestand (uit de alert)
-$path = "C:\Program Files\notepad++\notepad++.exe"
+$path = "C:\Users\MukiLukunaITSynergy\Downloads\UniLogicSetup_1_41_Build_213.exe"
 
 $sig  = Get-AuthenticodeSignature -FilePath $path
 $cert = $sig.SignerCertificate              # leaf cert
-$dest = "C:\Temp\Notepad++.cer"
+$dest = "C:\Temp\UniLogicSetup_1_41_Build_213.cer"
 Export-Certificate -Cert $cert -FilePath $dest | Out-Null
 $cert.Thumbprint
